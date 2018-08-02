070ea5ef3a7cdfbf6cee6b6bb87e6dc07f415077
“elgladiadordelcielo” <elgladiadordelcielo@gmail.com>
2018-08-02 15:34:58 +0200

    El servidor web ya funciona

--- blog_Miguel_Angel_Baez_Dominguez2/admin/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-session_start();
-$_SESSION['logueado']=false;
-header('Location: ../index.php');
-?>
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,7 +4,13 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
 class DefaultController extends Controller
 {
@@ -44,4 +50,32 @@ class DefaultController extends Controller
             )
         );
     }
+
+     /**
+     * @Route("/noticias.{_format}",
+     * name="noticias_xml",
+     * requirements={"_format": "json|xml"})     
+     */
+    public function noticiasJsonXmlAction($_format)
+    {
+
+        $repository = $this->getDoctrine()->getRepository('AppBundle:Noticias');
+
+        $noticias = $repository->findAllOrderedByFecha();
+
+        $encoders = array(new XmlEncoder(), new JsonEncoder());
+
+        $normalizers = array(new ObjectNormalizer());
+
+        $serializer = new Serializer($normalizers, $encoders);
+
+        $contenido = $serializer->serialize($noticias, $_format);
+
+        $response = new Response();
+
+        $response->headers->set('Contente-type','application/'.$_format);
+        $response->setContent($contenido);
+        return $response;
+    }
+
 }
